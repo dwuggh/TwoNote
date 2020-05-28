1b91ae9f6c3d4f51cdd3bb0c4b334c07a0feac2f
dwuggh <dwuggh@gmail.com>
2020-05-28 20:51:11 +0800

    init

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include "mainwindow.h"
+
+#include <KAboutData>
+#include <KLocalizedString>
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+  QApplication a(argc, argv);
+  KAboutData aboutData(
+      // The program name used internally. (componentName)
+      QStringLiteral("TwoNote"),
+      // A displayable program name string. (displayName)
+      i18n("TwoNote"),
+      // The program version string. (version)
+      QStringLiteral("0.0.1"),
+      // Short description of what the app does. (shortDescription)
+      i18n("whatever"),
+      // The license this code is released under
+      KAboutLicense::GPL,
+      // Copyright Statement (copyrightStatement = QString())
+      i18n("(c) 2015"),
+      // Optional text shown in the About box.
+      // Can contain any information desired. (otherText)
+      i18n("Some text..."),
+      // The program homepage string. (homePageAddress = QString())
+      QStringLiteral("http://example.com/"),
+      // The bug report email address
+      // (bugsEmailAddress = QLatin1String("submit@bugs.kde.org")
+      QStringLiteral("submit@bugs.kde.org"));
+  aboutData.addAuthor(i18n("dwuggh"), i18n("dwuggh"),
+                      QStringLiteral("dwuggh@gmail.com"),
+                      QStringLiteral("https://dwuggh.club"),
+                      QStringLiteral("OSC Username"));
+  KAboutData::setApplicationData(aboutData);
+  MainWindow w;
+  w.show();
+  return a.exec();
+}
