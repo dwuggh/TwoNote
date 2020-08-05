b8fa819d15ab6c6a27e89808f3ef853aa378c8bf
dwuggh <dwuggh@gmail.com>
2020-08-05 20:38:23 +0800

    revert

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -10,25 +10,24 @@ MainWindow::MainWindow(QWidget *parent)
     // setCentralWidget(new ScratchWidget(this));
     // setCentralWidget(new TCanvas(this));
 
-    // menubar = new QMenuBar(this);
-    // menubar->setObjectName(QString::fromUtf8("menubar"));
-    // menubar->setGeometry(QRect(0, 0, 720, 30));
+    menubar = new QMenuBar(this);
+    menubar->setObjectName(QString::fromUtf8("menubar"));
+    menubar->setGeometry(QRect(0, 0, 720, 30));
+    this->setMenuBar(menubar);
 
     toolbar = new QToolBar;
     toolbar->setObjectName(QString::fromUtf8("toolbar"));
     toolbar->setGeometry(QRect(0, 0, 720, 30));
-
     this->addToolBar(toolbar);
-    this->setMenuBar(menubar);
     
-    drawModeButton = new QToolButton;
-    drawModeButton->setText(tr("draw"));
-    drawModeButton->setCheckable(true);
-    drawModeButton->setChecked(true);
-    dragModeButton = new QToolButton;
-    dragModeButton->setText(tr("drag"));
-    dragModeButton->setCheckable(true);
-    dragModeButton->setChecked(false);
+    // drawModeButton = new QToolButton;
+    // drawModeButton->setText(tr("draw"));
+    // drawModeButton->setCheckable(true);
+    // drawModeButton->setChecked(true);
+    // dragModeButton = new QToolButton;
+    // dragModeButton->setText(tr("drag"));
+    // dragModeButton->setCheckable(true);
+    // dragModeButton->setChecked(false);
     
     
     // this->setStatusBar(new QStatusBar(this));
@@ -40,10 +39,10 @@ MainWindow::MainWindow(QWidget *parent)
     setCentralWidget(view);
     scene->setSceneRect(QRectF(-1920 / 2, 1080 / 2, 1920, 1080));
     view->show();
-    toolbar->addWidget(drawModeButton);
-    toolbar->addWidget(dragModeButton);
-    connect(drawModeButton, &QAbstractButton::toggled, view, &TView::enableDrawMode);
-    connect(dragModeButton, &QAbstractButton::toggled, view, &TView::enableDragMode);
+    toolbar->addWidget(view->drawModeButton);
+    toolbar->addWidget(view->dragModeButton);
+    // connect(view->drawModeButton, &QAbstractButton::toggled, this, &TView::enableDrawMode);
+    // connect(view->dragModeButton, &QAbstractButton::toggled, this, &TView::enableDragMode);
     //    ScratchScene* scene = new ScratchScene(view);
     //    // scene->addRect(QRectF(0, 0, 100, 100));
     //    view->setScene(scene);
